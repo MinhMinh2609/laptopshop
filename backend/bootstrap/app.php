06083bbe28ee4b43010d11fcdb481f5d53ec5af0
MinhMinh2609 <minh955371@gmail.com>
2026-06-28 15:50:45 +0700

    khong nhận được giao diện sản phẩm

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
 
         // Đăng ký alias middleware
         $middleware->alias([
